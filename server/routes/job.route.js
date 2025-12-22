2807bf1caa038d42b21d526dac1fe7145adf891d
sahilprajapati2005 <sahilprajapati0105@gmail.com>
2025-12-22 12:57:41 +0530

    fix bug

--- server/routes/job.route.js
+++ server/routes/job.route.js
@@ -1,8 +1,7 @@
 // In server/routes/job.route.js
 import express from "express";
-import isAuthenticated from "../middlewares/isAuthenticated.js";
+import isAuthenticated from "../middlewares/isauthenticated.js";
 import { getAdminJobs, getAllJobs, getJobById, postJob } from "../controller/job.controller.js";
-
 const router = express.Router();
 
 router.route("/post").post(isAuthenticated, postJob);
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,8 +1,7 @@
 import express from "express";
 import { login, logout, register, updateProfile } from "../controller/user.controller.js";
-import isAuthenticated from "../middlewares/isAuthenticated.js";
+import isAuthenticated from "../middlewares/isauthenticated.js"; 
 import { singleUpload } from "../middlewares/multer.js";
-
 const router = express.Router();
 
 router.route("/register").post(singleUpload, register); // Ensure singleUpload is here
